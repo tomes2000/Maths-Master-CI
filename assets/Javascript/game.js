4432278795901d05b8c1ffab3dd55a25dcd9b37c
tomes2000 <aavash2000@gmail.com>
2023-05-18 10:51:45 +0100

    adding database api easy, medium and hard

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,3 +1,8 @@
 // Create starting data list and quesitonNumberReference
 var data = {};
 let questionNumberReference = 0;
+
+// Databse API
+const easyQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=easy&type=multiple";
+const mediumQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=medium&type=multiple";
+const hardQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=hard&type=multiple";
