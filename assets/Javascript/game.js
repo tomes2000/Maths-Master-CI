c97dc72f5156543190ec09046faed247a38b4f51
tomes2000 <aavash2000@gmail.com>
2023-05-18 10:52:24 +0100

    adding difficulty selection and apiAdress format

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -6,3 +6,9 @@ let questionNumberReference = 0;
 const easyQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=easy&type=multiple";
 const mediumQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=medium&type=multiple";
 const hardQuiz = "https://opentdb.com/api.php?amount=20&category=17&difficulty=hard&type=multiple";
+
+// Difficulty selection
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium");
+const hard = document.getElementById("hard");
+let apiAddress;
