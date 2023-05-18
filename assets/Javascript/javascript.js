b991a50a6d843876891defacdeb97764f6701acc
tomes2000 <aavash2000@gmail.com>
2023-05-18 14:41:25 +0100

    implement modal concept to open and close

---
+++ assets/Javascript/javascript.js
@@ -0,0 +1,5 @@
+// Modal concept taken directly from --> "https://www.youtube.com/watch?v=XH5OW46yO8I"
+
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modalContainer = document.getElementById("modal-container");
