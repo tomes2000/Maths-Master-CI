42828221378c69cf52360cd78a874c9d8bb48514
tomes2000 <aavash2000@gmail.com>
2023-05-18 14:40:47 +0100

    add the high scores to the display list

--- assets/Javascript/highscores.js
+++ assets/Javascript/highscores.js
@@ -1,3 +1,8 @@
 // Top 10 highest scores
 const highScoresList = document.getElementById("highscoreslist");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+// Add to the high score list
+highScoresList.innerHTML = highScores.map(highScores => {
+    return `<li class="highscoreitem">${highScores.name} - ${highScores.score}</li>`;
+  }).join("");
