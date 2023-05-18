aa29610ed2451e526fb1b0e1fe07ba7a723583f2
tomes2000 <aavash2000@gmail.com>
2023-05-18 14:42:51 +0100

    modal open close event listeners

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -3,3 +3,13 @@
 const openModal = document.getElementById("open-modal");
 const closeModal = document.getElementById("close-modal");
 const modalContainer = document.getElementById("modal-container");
+
+// Open & close modal event listeners s
+openModal.addEventListener("click", () => {
+    modalContainer.classList.add("show");
+  });
+  
+  closeModal.addEventListener("click", () => {
+    modalContainer.classList.remove("show");
+  });
+  
